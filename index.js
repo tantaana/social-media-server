678213f0d5b8183685c281c2ab4eeed793813e95
tantaana <shariartanvirofficial@gmail.com>
2022-11-08 22:03:51 +0600

    another

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,6 +11,35 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.eivtc4s.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        const serviceName = client.db('docMike').collection('services')
+
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceName.find(query);
+            const services = await cursor.limit(3).toArray();
+            res.send(services)
+        })
+
+        app.get('/servicesAll', async (req, res) => {
+            const query = {};
+            const cursor = serviceName.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err))
 
 
 // -------------------------
